d90f5c492bd53c4a88e08da28d7fcfcbea65d6f8
Jeremy Van Grinsven <jeremy@vangrinsven.us>
2025-08-13 13:11:03 -0700

    Add bard keymaps

---
+++ keyboards/keychron/v6/ansi_encoder/keymaps/bard/keymap.c
@@ -0,0 +1,245 @@
+/* Copyright 2023 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+//#define TESTING
+
+#include <ymdk/ymd09/keymaps/macro.h>
+
+enum layers{
+  WIN_BASE,
+  WIN_FN,
+  _BARD_GRP,
+  _ENC_GRP,
+};
+
+enum my_keycodes {
+  TWIST2 = SAFE_RANGE,
+  TWIST3,
+  TWIST4,
+  TWIST_MANA,
+  HOLD2,
+  HOLD3,
+  AUTO2,
+  POP5,
+  POP6,
+  POP7,
+  POP8,
+  GGR_ROOT,
+  TRAINING1,
+  TRAINING2,
+  END_TWIST
+};
+
+// 30 min to reset to base layer.
+#define LAYER_TIMEOUT 1800000
+static uint32_t key_timer; 
+
+HoldRepeat* s_holds[] = { &m_h2, &m_h3, 0 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  key_timer = timer_read32(); // store time of last refresh
+
+  switch (keycode) {
+    case TWIST2:
+      if (record->event.pressed) {
+        start_twist(&m_twist2, record, true);
+      }
+      return false;
+    case TWIST3:
+      if (record->event.pressed) {
+        start_twist(&m_twist3, record, true);
+      }
+      return false;
+    case TWIST4:
+      if (record->event.pressed) {
+        start_twist(&m_twist4, record, true);
+      }
+      return false;
+    case TWIST_MANA:
+      if (record->event.pressed) {
+        start_twist(&m_twist_mana, record, true);
+      }
+      return false;
+    case HOLD2:
+      if (record->event.pressed) {
+        start_hold_repeat(&m_h2, record);
+      } else {
+        release_hold_repeat(&m_h2);
+      }
+      return false;
+    case HOLD3:
+      if (record->event.pressed) {
+        start_hold_repeat(&m_h3, record);
+      } else {
+        release_hold_repeat(&m_h3);
+      }
+      return false;
+    case AUTO2:
+      if (record->event.pressed) {
+        start_twist(&m_auto2, record, false);
+      }
+      return false;
+    case END_TWIST:
+      if (record->event.pressed) {
+        stop_twist();
+      }
+      return false;
+    case POP5:
+      if (record->event.pressed) {
+        start_pop(&m_pop5);
+      }
+      return false;
+    case POP6:
+      if (record->event.pressed) {
+        start_pop(&m_pop6);
+      }
+      return false;
+    case POP7:
+      if (record->event.pressed) {
+        start_pop(&m_pop7);
+      }
+      return false;
+    case POP8:
+      if (record->event.pressed) {
+        start_pop(&m_pop8);
+      }
+      return false;
+    case GGR_ROOT:
+      if (record->event.pressed) {
+        start_macro(&m_charm_break_root);
+      }
+      return false;
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
+void keyboard_post_init_user(void) {
+  // Set all keys to off.
+  //rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+  //rgb_matrix_sethsv_noeeprom(HSV_OFF);
+
+  macro_post_init_user();
+
+  key_timer = timer_read32(); // Start key timer.
+}
+
+bool rgb_matrix_indicators_user(void) {
+  // Light up choose buttons.
+  if (IS_LAYER_ON(WIN_FN)) {
+    rgb_matrix_set_color(16, 0, 0, 255);
+    rgb_matrix_set_color(17, 0, 0, 255);
+    rgb_matrix_set_color(18, 0, 0, 255);
+    return true;
+  }
+
+  // Clear the backlight on the mode keys.
+  rgb_matrix_set_color(16, 0, 0, 0);
+  rgb_matrix_set_color(17, 0, 0, 0);
+  rgb_matrix_set_color(18, 0, 0, 0);
+  rgb_matrix_set_color(19, 0, 0, 0);
+
+  // Subtle light up the layer indicator.
+  if (IS_LAYER_ON(_BARD_GRP)) {
+    rgb_matrix_set_color(17, 0, 0, 255);
+  } else if (IS_LAYER_ON(_ENC_GRP)) {
+    rgb_matrix_set_color(18, 0, 0, 255);
+  } else {
+    rgb_matrix_set_color(16, 0, 0, 255);
+  }
+
+  if (m_twist) {
+    // Light up the current twist key.
+    rgb_matrix_set_color(g_led_config.matrix_co[twist_keypos.row][twist_keypos.col], 255, 255, 255);
+    // Light up the stop song key.
+    rgb_matrix_set_color(19, 255, 0, 0);
+  }
+  for (int i=0; s_holds[i]; i++) {
+    if (s_holds[i]->active) {
+      // Light up the hold key.
+      rgb_matrix_set_color(g_led_config.matrix_co[s_holds[i]->keypos.row][s_holds[i]->keypos.col], 255, 255, 255);
+    }
+  }
+  return true;
+}
+
+void matrix_scan_user(void) {
+  static int macros_len = 3;
+  Macro* macros[] = { &m_charm_break_root, m_twist, m_pop };
+
+  for (int i=0; i < macros_len; i++) {
+    if (macros[i]) {
+      step_macro(macros[i]);
+    }
+  }
+
+  for (int i=0; s_holds[i]; i++) {
+    step_hold_repeat(s_holds[i]);
+  }
+}
+
+void housekeeping_task_user(void) {
+    if (!IS_LAYER_ON(WIN_BASE) && timer_elapsed32(key_timer) > LAYER_TIMEOUT)
+    {
+        layer_move(WIN_BASE);
+    }
+}
+
+
+#define KC_TASK LGUI(KC_TAB)
+#define KC_FLXP LGUI(KC_E)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [WIN_BASE] = LAYOUT_ansi_109(
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,  KC_PSCR,  KC_NO,    RGB_MOD,  _______,  _______,  _______,  _______,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
+    [WIN_FN] = LAYOUT_ansi_109(
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,  _______,  _______,  RGB_TOG,  TO(WIN_BASE), TO(_BARD_GRP), TO(_ENC_GRP), _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                _______,  _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,            _______,            _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,            _______,  _______),
+    [_BARD_GRP] = LAYOUT_ansi_109(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  END_TWIST,
+        _______,  _______,  HOLD2,    HOLD3,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                TWIST2,   _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,            TWIST3,   TWIST4,   TWIST_MANA,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  AUTO2,              POP8,     _______),
+    [_ENC_GRP] = LAYOUT_ansi_109(
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  GGR_ROOT, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                _______,  _______,  _______,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,            _______,  _______,  _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,            _______,  _______)
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_FN]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI) },
+    [_BARD_GRP] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_ENC_GRP] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif // ENCODER_MAP_ENABLE
---
+++ keyboards/ymdk/ymd09/keymaps/bard/keymap.c
@@ -0,0 +1,153 @@
+#include QMK_KEYBOARD_H
+
+//#define TESTING
+
+#include "../macro.h"
+
+enum my_keycodes {
+  TWIST2 = SAFE_RANGE,
+  TWIST3,
+  TWIST4,
+  TWIST_MANA,
+  POP5,
+  POP6,
+  POP7,
+  POP8,
+  TRAINING1,
+  TRAINING2,
+  END_TWIST
+};
+
+enum layer_names {
+    _BARD_GRP,
+    _BARD_EXP,
+    _TRAINING,
+    _LAYER_CHOOSE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case TWIST2:
+      if (record->event.pressed) {
+        start_twist(&m_twist2, record, true);
+      }
+      return false;
+    case TWIST3:
+      if (record->event.pressed) {
+        start_twist(&m_twist3, record, true);
+      }
+      return false;
+    case TWIST4:
+      if (record->event.pressed) {
+        start_twist(&m_twist4, record, true);
+      }
+      return false;
+    case TWIST_MANA:
+      if (record->event.pressed) {
+        start_twist(&m_twist_mana, record, true);
+      }
+      return false;
+    case END_TWIST:
+      if (record->event.pressed) {
+        stop_twist();
+      }
+      return false;
+    case POP5:
+      if (record->event.pressed) {
+        start_pop(&m_pop5);
+      }
+      return false;
+    case POP6:
+      if (record->event.pressed) {
+        start_pop(&m_pop6);
+      }
+      return false;
+    case POP7:
+      if (record->event.pressed) {
+        start_pop(&m_pop7);
+      }
+      return false;
+    case POP8:
+      if (record->event.pressed) {
+        start_pop(&m_pop8);
+      }
+      return false;
+    case TRAINING1:
+      if (record->event.pressed) {
+        start_twist(&training1, record, false);
+      }
+      return false;
+    case TRAINING2:
+      if (record->event.pressed) {
+        start_twist(&training2, record, false);
+      }
+      return false;
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
+
+void keyboard_post_init_user(void) {
+  // Set all keys to off.
+  rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+  rgb_matrix_sethsv_noeeprom(HSV_OFF);
+
+  macro_post_init_user();
+}
+
+bool rgb_matrix_indicators_user(void) {
+  // Light up choose buttons.
+  if (IS_LAYER_ON(_LAYER_CHOOSE)) {
+    rgb_matrix_set_color(g_led_config.matrix_co[0][2], 0, 0, 255);
+    rgb_matrix_set_color(g_led_config.matrix_co[0][0], 0, 0, 255);
+    return true;
+  }
+
+  // Subtle light up the layer indicator.
+  if (IS_LAYER_ON(_BARD_EXP)) {
+    rgb_matrix_set_color(g_led_config.matrix_co[0][2], 0, 0, 15);
+  } else if (IS_LAYER_ON(_TRAINING)) {
+    rgb_matrix_set_color(g_led_config.matrix_co[1][2], 0, 0, 15);
+  } else {
+    rgb_matrix_set_color(g_led_config.matrix_co[0][0], 0, 0, 15);
+  }
+
+  if (m_twist) {
+    // Light up the current twist key.
+    rgb_matrix_set_color(g_led_config.matrix_co[twist_keypos.row][twist_keypos.col], 0, 255, 0);
+    // Light up the stop song key.
+    rgb_matrix_set_color(g_led_config.matrix_co[0][0], 255, 0, 0);
+  }
+  return true;
+}
+
+void matrix_scan_user(void) {
+  int macros_len = 3;
+  Macro* macros[] = { m_twist, m_pop };
+
+  for (int i=0; i < macros_len; i++) {
+    if (macros[i]) {
+      step_macro(macros[i]);
+    }
+  }
+}
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_BARD_GRP] = LAYOUT(END_TWIST, LT(_LAYER_CHOOSE, KC_KP_8), TWIST2,
+                       TWIST3, TWIST4, TWIST_MANA, 
+                       POP7, POP8, KC_1),
+
+  [_BARD_EXP] = LAYOUT(END_TWIST, _______, TWIST2,
+                       TWIST3, POP6, KC_7, 
+                       KC_Q, POP5, KC_1),
+
+  [_TRAINING] = LAYOUT(END_TWIST, _______, KC_9,
+                       KC_4, KC_5, KC_6, 
+                       TRAINING1, TRAINING2, KC_3),
+
+  [_LAYER_CHOOSE] = LAYOUT(TO(_BARD_GRP), _______, TO(_BARD_EXP),
+                           _______, _______, TO(_TRAINING), 
+                           _______, _______, _______),
+};
